7f62dc67d5eb22d5e52b093a5842fcd3a089f7e9
silvia.laura.pintea <silvia.laura.pintea@d0ac619c-fcb0-45f4-6102-1287495a1daa>
2010-07-29 14:44:24 +0000

    some examples from our previous code to call behaviors and retrieve images from Nao's camera

---
+++ get_nao_behaviors.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import time
+from PIL import Image
+import cv
+import imghdr
+import os
+import sys
+import random
+#PATHS FOR NAO___________________________________________________________
+alPath = "/data/Documents/semester2/AIProject/nao/lib"
+#alPath = "C:\Program Files\Aldebaran\Choregraphe 1.6.13\lib"
+sys.path.append(alPath)
+import naoqi
+from naoqi import ALBroker
+from naoqi import ALModule
+from naoqi import ALProxy
+from naoqi import ALBehavior         
+
+#_________________________________________________________________________
+#_________________________________________________________________________
+class getNaoBehaviors:
+	def __init__(self, host, port):
+		self.host     = host #"192.168.0.80"
+		self.port     = port #9559
+		self.frame    = None
+		self.motion   = None
+		self.basePath = "/home/nao/behaviors/"
+
+	#INITIALIZE THE VIDEO DEVICE_____________________________________________
+	def initDevice():
+		self.frame  = ALProxy("ALFrameManager", host, port)
+		self.motion = ALProxy("ALMotion", host, port)
+
+	#CALL THE NEEDED BEHAVIOR________________________________________________
+	def callBehavior(what):
+		gesture_path = self.basePath + what
+		gesture_id   = self.frame.newBehaviorFromFile(gesture_path, "")
+
+		self.motion.stiffnessInterpolation("Body", 1.0, 1.0) # ... parameters?
+		self.frame.playBehavior(gesture_id)
+		self.frame.completeBehavior(gesture_id) # ... what does it do?
+		
+
---
+++ get_nao_image.py
@@ -0,0 +1,67 @@
+import os
+import sys
+import time
+from PIL import Image
+import cv
+import imghdr
+import os
+import sys
+import random
+#PATHS FOR NAO___________________________________________________________
+alPath = "/data/Documents/semester2/AIProject/nao/lib"
+#alPath = "C:\Program Files\Aldebaran\Choregraphe 1.6.13\lib"
+sys.path.append(alPath)
+import naoqi
+from naoqi import ALBroker
+from naoqi import ALModule
+from naoqi import ALProxy
+from naoqi import ALBehavior         
+
+#_________________________________________________________________________
+#_________________________________________________________________________
+class getNaoImage:
+	def __init__(self, host, port):
+		self.host        = host #"192.168.0.80"
+		self.port        = port #9559
+		self.videoDevice = None
+		self.gvm         = None
+
+	#INITIALIZE THE VIDEO DEVICE_____________________________________________
+	def initDevice():
+		#CONNECT TO A PROXY
+		try:
+		    self.videoDevice = ALProxy("ALVideoDevice", self.host, self.port)
+		except Exception, e:
+		    print "Error when creating video device proxy:"+str(e)
+		    exit(1)
+
+		#SUBSCRIBE GVM TO VIM
+		self.gvm = self.videoDevice.subscribe("GVM", 0, 13, 1) # ... parameters?
+
+	#RETRIEVE IMAGES__________________________________________________________
+	def getImage():
+		#GET THE IMAGE BYTES
+	    result = self.videoDevice.getImageRemote(self.gvm)
+		
+		#PROCESS RESULT TO GET THE IMAGE
+    	imString = result[6]
+    	columns  = result[0]
+    	rows     = result[1]
+
+    	#TRANSFORM THE STRING IN IMAGE
+    	im     = Image.fromstring("RGB", (columns, rows), imString)
+    	cv_img = cv.CreateImageHeader(im.size, cv.IPL_DEPTH_8U, 3)
+    	cv.SetData(cv_img, im.tostring(), im.size[0]*3)
+    	return cv_img
+
+ 	#CLOSE VIDEO DEVICE_________________________________________________________
+	def closeDevice():
+    	#UNSUBSCRIBE THE IMAGE DEVICE - UNSUBSCRIBE SVM TO VIM
+		self.videoDevice.unsubscribe(self.gvm)
+
+
+
+
+
+
+
