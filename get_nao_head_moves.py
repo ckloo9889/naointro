6895ce21917278a8c7bad936d4aff22b4a6d4a46
silvia.laura.pintea <silvia.laura.pintea@d0ac619c-fcb0-45f4-6102-1287495a1daa>
2010-08-10 20:58:28 +0000

    exception catching in head_moves class

---
+++ get_nao_head_moves.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import time
+from PIL import Image
+import cv
+import imghdr
+import os
+import sys
+import random
+import math
+#PATHS FOR NAO___________________________________________________________
+#alPath = "/data/Documents/nao/lib"
+alPath = "C:\Program Files\Aldebaran\Choregraphe 1.6.13\lib"
+sys.path.append(alPath)
+import naoqi
+from naoqi import ALBroker
+from naoqi import ALModule
+from naoqi import ALProxy
+from naoqi import ALBehavior         
+from naoqi import motion
+
+#_________________________________________________________________________________________________
+#_________________________________________________________________________________________________
+class getNaoHeadMoves:
+	def __init__(self, host, port):
+		self.host         = host # "192.168.0.80"
+		self.port         = port # 9559
+		self.motionDevice = None
+		self.memoryDevice = None
+		self.speechDevice = None
+		self.stiffness    = 1.0
+		self.nrJoints     = 0
+
+	#INITIALIZE THE MOTION DEVICE__________________________________________________________________
+	def initDevice(self):
+		#CONNECT TO A PROXY
+		try:
+		    self.motionDevice = ALProxy("ALMotion", self.host, self.port)
+		except Exception, e:
+		    print "Error when creating motion device proxy:"+str(e)
+		    exit(1)
+
+		#MAKE NAO STIFF (OTHERWISE IT WON'T MOVE)
+		try:
+			self.motionDevice.stiffnessInterpolation("Body",self.stiffness,1.0)
+		except Exception, e:
+		    print "Error when making nao stiff: "+str(e)
+
+	#MOVE HEAD YAW_________________________________________________________________________________
+	def moveHeadYawAbs(self, pTargetAngles, timeLists):
+		names  = "HeadYaw"
+		#CONVERT TO RADIANS
+		pTargetAngles = [x * (math.pi/180.0) for x in pTargetAngles]
+		isAbsolute  = True
+		try:
+			self.motionDevice.angleInterpolation(names, pTargetAngles, timeLists, isAbsolute)
+		except Exception, e:
+		    print "Error when changing the head yaw: "+str(e)
+
+	#MOVE HEAD PITCH________________________________________________________________________________
+	def moveHeadPitchAbs(self, pTargetAngles, timeLists):
+		names  = "HeadPitch"
+		pTargetAngles = [x * (math.pi/180.0) for x in pTargetAngles]
+		#CONVERT TO RADIANS
+		pTargetAngles = [x * (math.pi/180.0) for x in pTargetAngles]
+		isAbsolute  = True
+		try:
+			self.motionDevice.angleInterpolation(names, pTargetAngles, timeLists, isAbsolute)
+		except Exception, e:
+		    print "Error when changing the head pitch: "+str(e)
+
+		
