dbd1e55458dd5577a0b993852b476346cb5d5a65
jturnhout <jturnhout@d0ac619c-fcb0-45f4-6102-1287495a1daa>
2010-08-11 13:07:22 +0000

    indents fixed

--- trunk/get_nao_behaviors.py
+++ trunk/get_nao_behaviors.py
@@ -15,15 +15,15 @@ import naoqi
 from naoqi import ALBroker
 from naoqi import ALModule
 from naoqi import ALProxy
-from naoqi import ALBehavior         
+from naoqi import ALBehavior		 
 
 #_________________________________________________________________________
 #_________________________________________________________________________
 class getNaoBehaviors:
 	def __init__(self, host, port):
-		self.host     = host # "192.168.0.80"
-		self.port     = port # 9559
-		self.frame    = None
+		self.host	 = host # "192.168.0.80"
+		self.port	 = port # 9559
+		self.frame	= None
 		self.motion   = None
 		#SET PATH TO BEHAVIORS IN NAO'S HEAD
 		self.basePath = "/home/nao/behaviors/" #"/data/Documents/nao/projects/" 
@@ -33,13 +33,13 @@ class getNaoBehaviors:
 		try:
 			self.frame  = ALProxy("ALFrameManager", self.host, self.port)
 		except Exception, e:
-		    print "Error when creating the frame manager device: "+str(e)
+			print "Error when creating the frame manager device: "+str(e)
 			exit(1)
 
 		try:
 			self.motion = ALProxy("ALMotion", self.host, self.port)
 		except Exception, e:
-		    print "Error when creating the motion device: "+str(e)
+			print "Error when creating the motion device: "+str(e)
 			exit(1)
 		
 	#CALL THE NEEDED BEHAVIOR________________________________________________
@@ -48,14 +48,14 @@ class getNaoBehaviors:
 		try:
 			gesture_id   = self.frame.newBehaviorFromFile(gesture_path, "")
 		except Exception, e:
-		    print "Error when reading the behavior from file: "+str(e)
+			print "Error when reading the behavior from file: "+str(e)
 
 		#THE BODY NEEDS TO BE STIFF BEFORE PLAYING THE BAHVIORS!
  		try:
 			self.frame.playBehavior(gesture_id)
 			self.frame.completeBehavior(gesture_id) # ... what does it do?
 		except Exception, e:
-		    print "Error when playing the behavior: "+str(e)
+			print "Error when playing the behavior: "+str(e)
 	
 		
 
